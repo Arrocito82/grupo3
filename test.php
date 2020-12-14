24cbf107e458b2cf3c2f311c3e215395409d7832
andrea <moralesandrea.edr4@gmail.com>
2020-12-13 19:10:24 -0600

    ya funciona

--- test.php
+++ test.php
@@ -7,15 +7,26 @@
 	
 	
 	<?php
+require 'vendor/autoload.php' ;
+$client = new MongoDB\Client(
+    'mongodb+srv://admin:grupo03TPI@grupo03.wwsio.mongodb.net/grupo03?retryWrites=true&w=majority'
+);
 
-	if()
-	$client=new MongoDB\(
-	'');
-	
-	
 
+$collection = $client->grupo03->Categoria;
+
+//$result = $collection->insertOne( [ 'nombre' => 'bailable' ] );
+
+//echo "Inserted with Object ID '{$result->getInsertedId()}'";
+//$cursor = $collection->find(['nombre' => 'bailable']);
+$cursor = $collection->find([]);
+
+foreach ($cursor as $document) {
+    echo $document['_id'], " \t",$document['nombre'], "<br>";
+}
 	?> 
 	<h1>algo es algo</h1>
 
 </body>
 </html>
+
