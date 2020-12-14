7cdd76a065b41d405a86b5ab5e7c1e85af12ba4a
Arrocito82 <moralesandrea.edr4@gmail.com>
2020-12-13 18:33:06 -0600

    test

---
+++ test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test</title>
+</head>
+<body>
+	
+	
+	<?php
+
+	if()
+	$client=new MongoDB\(
+	'');
+	
+	
+
+	?> 
+	<h1>algo es algo</h1>
+
+</body>
+</html>
