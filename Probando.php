94f886ef620cbdcb1dcf57cc088a5a368e23e569
adolfo199 <edgareeel@gmail.com>
2020-12-28 13:51:40 -0600

    gg

--- Probando.php
+++ Probando.php
@@ -5,6 +5,7 @@ require "Components/header.php";
 echo "<br>";
 //echo AutorRepo::CrearAutor("Prueba2");
 echo var_dump(AutorRepo::ObtenerAutoresPorNombre("Prueba"));
+
 ?>
 <script>
     document.getElementsByTagName("nav")[0].style.display="none";
