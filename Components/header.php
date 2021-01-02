f971ee0e49d05b8a2160f5381ec602e747d38aaa
Eduardo Orellana <og17006@ues.edu.sv>
2021-01-02 12:21:38 -0600

    Favicon

--- Components/header.php
+++ Components/header.php
@@ -39,7 +39,7 @@ $flag = isset($User);
         <?php echo $title ?>
     </title>
     <!-- FAVICON -->
-    <link rel="shortcut icon" type="image/ico" href="public/icono/favicon.ico" />
+    <link rel="shortcut icon" type="image/ico" href="public/icono/favicon_aud.ico" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="public/css/estilo.css">
     <link rel="stylesheet" href="<?= $style ?>">
