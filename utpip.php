31af9f19a7f5903b1be29d17ee3ddcb83ce3894a
unknown <edgareeel@gmail.com>
2021-01-03 12:42:35 -0600

    updater

--- utpip.php
+++ utpip.php
@@ -1,8 +1,9 @@
 <?php 
+    use Components\Alert;
     if(!isset($_GET['hjsd']))
         header("Location: index.php");
     if($_GET['hjsd']!="ghdsnd4456sxbas")
         header("Location: index.php");
     
-        echo exec("git pull");
+        echo Alert::SimpleAlert(exec("git pull") , "alert alert-success");
 ?>
