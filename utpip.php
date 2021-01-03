c6aabd8ad7c485e443a1d52afc3c226a24462799
unknown <edgareeel@gmail.com>
2021-01-03 12:14:56 -0600

    updater

---
+++ utpip.php
@@ -0,0 +1,8 @@
+<?php 
+    if(!isset($_GET['hjsd']))
+        header("Location: index.php");
+    if($_GET['hjsd']!="ghdsnd4456sxbas")
+        header("Location: index.php");
+    
+        echo shell_exec("git pull");
+?>
