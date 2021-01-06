8691e4ff7f06bf15db339e9c1920fc827bb4dbda
adolfo199 <edgareeel@gmail.com>
2021-01-06 16:52:10 -0600

    nada

--- Components/header.php
+++ Components/header.php
@@ -53,7 +53,7 @@ $flag = isset($User);
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
 
         <div class="container">
-            <a class="navbar-brand" href="/">Navbar</a>
+            <a class="navbar-brand" href="/">AudaFreeMp3</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
