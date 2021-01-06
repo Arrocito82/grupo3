8b191331dc774a9fa84435069798c27c0f336053
adolfo199 <edgareeel@gmail.com>
2021-01-06 15:55:56 -0600

    nada

--- Buscar.php
+++ Buscar.php
@@ -5,7 +5,7 @@
     use Repositories\AutorRepo;
     use Components\SearchTable;
 
-
+    echo '<div class="container">';
     if(isset($_POST['f'])){
                         
         $filtro =['titulo' =>  new \MongoDB\BSON\Regex(preg_quote($_POST['f']), 'i')];
@@ -19,9 +19,10 @@
         
         echo $htmlResult->audios;
         echo $htmlResult->autores;
+        
     }
     else{
-        echo '<div class="align-content-center container px-5"><form class="form-inline my-2 my-lg-0" Action="Buscar.php" method="post">
+        echo '<div class="align-content-center px-5"><form class="form-inline my-2 my-lg-0" Action="Buscar.php" method="post">
                     <input class="form-control mr-sm-2 " type="search" placeholder="Search" aria-label="Search" name="f" size="40">
                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form></div>';
@@ -29,5 +30,6 @@
                 document.getElementById('navSearch').style.display = 'none';
             </script>";
     }
+    echo '</div>';
 ?>
 
