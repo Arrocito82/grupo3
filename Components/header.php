d7f6db037e42afe4aea80bb3b9f36b239edb0b98
adolfo199 <edgareeel@gmail.com>
2021-01-06 16:45:49 -0600

    nada

--- Components/header.php
+++ Components/header.php
@@ -53,7 +53,7 @@ $flag = isset($User);
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
 
         <div class="container">
-            <a class="navbar-brand" href="<?php echo $path; ?>">Navbar</a>
+            <a class="navbar-brand" href="/">Navbar</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -62,7 +62,7 @@ $flag = isset($User);
 
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link " href="<?php echo $path; ?>">Inicio <span class="sr-only">(current)</span></a>
+                        <a class="nav-link " href="/">Inicio <span class="sr-only">(current)</span></a>
                     </li>
 
                     <?php
