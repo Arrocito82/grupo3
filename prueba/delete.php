8ecd1c274098835daed65618cca22bbfc6ca42c5
andrea <moralesandrea.edr4@gmail.com>
2021-01-07 20:29:36 -0600

    se elimino la carpeta de prueba

--- prueba/delete.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    if(isset($_GET['file'])){
-        system("sudo rm ." . $_GET['file']);
-    }
-?>
--- prueba/post.php
+++
@@ -1,8 +0,0 @@
-<?php
-$data = json_decode(file_get_contents('php://input'), true);
-
-
-
-echo var_dump($data);
-
-?>
